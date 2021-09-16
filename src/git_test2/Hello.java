729469a75856e9ce74cab9463ad836b6c87163d1
kim <rlayjean0@gmail.com>
2021-09-16 21:27:27 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -4,10 +4,12 @@ public class Hello {
     
     String message = "안녕하세요";
 
-    public void say() {
-        System.out.println(message);
+    public String say() {
+        return message;
     }
 
 }
+
+
 
 
